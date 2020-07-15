61e0fc453f92ab8dc009b0243784de9770647179
Charles Perry <charles@Charless-MacBook-Pro.local>
2020-07-15 16:42:21 -0700

    Part 2 complete after demo in class

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import FilmDetails from './FilmDetail'
 import FilmListings from './FilmListing'
@@ -7,19 +6,10 @@ import TMDB from './TMDB'
 class App extends Component {
   render() {
     return (
-      <div className="App">
         <div className="film-library">
-          <div className="film-list">
-            <h1 className="section-title">FILMS</h1>
             <FilmListings films={TMDB.films} />
             <FilmDetails films={TMDB.films} />
-          </div>
-
-          <div className="film-details">
-            <h1 className="section-title">DETAILS</h1>
-          </div>
         </div>
-      </div>
     );
   }
 }
