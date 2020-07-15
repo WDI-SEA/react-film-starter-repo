76fc54fd3233c9b6818970bca4e035e298d141b8
smcleod86 <mcleod.scott_86@yahoo.com>
2020-07-15 14:41:03 -0700

    complete part 1

---
+++ src/FilmRow.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import FilmPoster from './FilmPoster'
+
+class FilmRow extends Component {
+    render() {
+        return (
+            <div className="film-row">
+                <FilmPoster 
+                    src={`http://images.tmdb.org/t/p/w780${this.props.film.poster_path}`}
+                    alt={`Poster for the film ${this.props.film.title}`}
+                />
+
+                <div className="film-summary">
+                    <h1>{this.props.film.title}</h1>
+                    <p>{this.props.film.release_date.substring(0, 4)}</p>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default FilmRow;
