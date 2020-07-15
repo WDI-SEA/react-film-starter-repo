44639d36a9246ad0431ee3708f436d580fabb2ae
Joshua Danao <josh@Joshuas-MacBook-Air.local>
2020-07-14 18:18:24 -0700

    forgot to git commit

---
+++ src/FilmRow.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import FilmPoster from './FilmPoster'
+
+class FilmRow extends Component {
+  render() {
+    return (
+      <div className="film-row">
+        <FilmPoster poster_url={`https://image.tmdb.org/t/p/w780${this.props.film.poster_path}`} title={this.props.film.title} />        
+        <div className="film-summary">
+          <h1>{this.props.film.title}</h1>
+          <p>{this.props.film.release_date}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FilmRow;
