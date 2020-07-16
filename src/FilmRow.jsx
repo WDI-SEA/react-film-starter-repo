10855d0e31c71687f5438c3618155a1a8ee08352
Nick Hvattum <nhvattum@gmail.com>
2020-07-15 17:10:43 -0700

    finished parts one and two in class.  I dont get this

---
+++ src/FilmRow.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import FilmPoster from './FilmPoster';
+import Fave from './Fave';
+
+class FilmRow extends Component {
+    handleDetailsClick = (film) => {
+        console.log(`Fetching details for ${film.title} 🤬`)
+    }
+
+    render() {
+        return (
+            <div className="film-row" onClick={() => this.handleDetailsClick(this.props.film)}>
+                <FilmPoster
+                    src={`https://images.tmdb.org/t/p/w780${this.props.film.poster_path}`}
+                    alt={`Poster for the film ${this.props.film.title}`}
+                />
+               
+                <div className="film-summary">
+                    <h1>{this.props.film.title}</h1>
+                    <p>{this.props.film.release_date.substring(0, 4)}</p>
+                </div>
+                <Fave />
+            </div>
+        );
+    }
+}
+
+export default FilmRow
+;
