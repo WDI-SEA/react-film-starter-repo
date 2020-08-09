c30f849bb316e5263c8095af81e40b9d83f91b93
Emily Krauss <e.krauss27@gmail.com>
2020-08-09 12:31:57 -0700

    posters show up

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
-
 class FilmRow extends Component {
     render() {
-        const posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.poster_path}`
+        const posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.poster}`
         const fullyear = this.props.year.split("-", 1)
         return (
         <div className="film-row">
           <h1>{this.props.film}</h1>
-          <img src={posterUrl} alt="" />
+          <img src={posterUrl} alt="movie-poster" />
           <div className="film-summary">
             <p>{fullyear}</p>
           </div>
