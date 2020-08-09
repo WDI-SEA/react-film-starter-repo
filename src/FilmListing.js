79003c9a8499c82ff035f097e246942c82cbad01
Emily Krauss <e.krauss27@gmail.com>
2020-08-09 12:12:45 -0700

    all films show up

---
+++ src/FilmListing.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class FilmListing extends Component {
+    render() {
+    const allFilms = this.props.films.map((film) => {
+        return (
+            <div className="film-row">
+                <h1>{film.title}</h1>
+            </div>
+        )
+    }) 
+      return (
+      <div className="film-list">
+        <h1 className="section-title">FILMS</h1>
+        {allFilms}
+      </div>
+      );
+    }
+}
+
+export default FilmListing;
