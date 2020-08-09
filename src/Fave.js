1c3893c0bd9cb03fdc2d5c6469cdb92ee869734b
Emily Krauss <e.krauss27@gmail.com>
2020-08-09 14:14:55 -0700

    sett filter on click functions for faves and all

--- src/Fave.js
+++ src/Fave.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class Fave extends Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+        console.log('Click happened');
+      }
     render() {
         return (
-          <div className="film-row-fave add_to_queue">
+          <div className="film-row-fave add_to_queue" onClick={this.handleClick}>
             <p className="material-icons">add_to_queue</p>
           </div>  
         );
--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import FilmRow from './FilmRow'
 
 class FilmListing extends Component {
+    constructor(props) {
+        super(props);
+        this.handleFilterClick = this.handleFilterClick.bind(this)
+    }
+    handleFilterClick(filter) {
+        console.log(`Setting filter to: ${filter}`)
+    }
     render() {
     const allFilms = this.props.films.map((film) => {
         return (
@@ -11,6 +18,17 @@ class FilmListing extends Component {
       return (
       <div className="film-list">
         <h1 className="section-title">FILMS</h1>
+        <div className="film-list-filters">
+            <div className="film-list-filter" onClick={() => this.handleFilterClick('all')}>
+                ALL
+                <span className="section-count">{this.props.films.length}</span>
+            </div>
+            <div className="film-list-filter" onClick={() => this.handleFilterClick('faves')}>
+                FAVES
+                <span className="section-count">0</span>
+            </div>
+        </div>
+
         {allFilms}
       </div>
       );
