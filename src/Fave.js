39b15089707ac5d845903667c5c9c9d97136d04c
yasaman forouzesh <yasamanforouzesh93@gmail.com>
2021-02-03 21:58:25 -0800

    fixed

--- src/Fave.js
+++ src/Fave.js
@@ -1,17 +1,17 @@
 import React, {Component} from 'react'
 
 class Fave extends Component{
-    constructor(){
-        // This binding is necessary to make `this` work in the callback
-        this.handleClick = this.handleClick.bind(this)
-    }
+    // constructor(){
+    //     // This binding is necessary to make `this` work in the callback
+    //     this.handleClick = this.handleClick.bind(this)
+    // }
     stae={
         isFave:false
     }
     handleClick=(e)=>{
         e.stopPropagation()
         console.log("I'm clicking")
-        this.setState
+       
     }
     render(){
         return(
