a1a609a7b1b399b7a8ca3697077304a0028960b7
Blake DeGraw <bbdegraw@gmail.com>
2021-02-03 20:49:01 -0800

    step 7 done

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import TMDB from './TMDB'
 
 class FilmRow extends Component {
     render() {
         return(
             <div className="film-row">
+                <img src={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`} />
                 <h1>{this.props.film.title}</h1>
             </div>
         )
