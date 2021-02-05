1283a680c37a5c2925024dac6c7247686a45fe18
Blake DeGraw <bbdegraw@gmail.com>
2021-02-04 16:57:13 -0800

    pt 2: pt 2 step 1 done

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import FilmRow from './FilmRow'
 
 class FilmListing extends Component {
+
+    handleFilterClick = (filter) => {
+        console.log('Setting filter to' + filter)
+    }
+
     render() {
         let allFilms = this.props.films.map((film, i) => {
             return <FilmRow film={film} key={i}/>
