bb49bb56331e4f3caaa9f65844afe8b08220fa4b
Blake DeGraw <bbdegraw@gmail.com>
2021-02-04 17:18:59 -0800

    pt2: pt3 step 2 done

--- src/FilmRow.js
+++ src/FilmRow.js
@@ -10,8 +10,7 @@ class FilmRow extends Component {
 
     render() {
         return(
-            <div className="film-row">
-                {/* <img src={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`} /> */}
+            <div onClick={this.handleDetailsClick} className="film-row">
                 <FilmPoster poster={this.props.film.poster_path} />
                 <div className="film-summary">
                     <h1>{this.props.film.title}</h1>
