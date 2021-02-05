5c928477d982eed57a61c11ffa992ff370176924
Drake Tyksinski <drake.tyksinski@live.com>
2021-02-05 09:19:07 -0800

    almost finished

---
+++ src/FilmListing.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import FilmRow from './FilmRow'
+
+class FilmListing extends Component {
+
+    handleFilterClick = (filter) => {
+        console.log(`Setting filter to ${filter}`)
+    }
+
+    render() {
+        const allFilms = this.props.films.map((film, index) => {
+            return (
+                <FilmRow film={film} key={`key${index}`} />
+            )
+        })
+        return (
+            <div className="film-list">
+                <h1 className="section-title">FILMS</h1>
+                <div className="film-list-filters">
+                    <div onClick={() => this.handleFilterClick('all')}className="film-list-filter">
+                        ALL
+            <span className="section-count">{this.props.films.length}</span>
+                    </div>
+                    <div onClick={() => this.handleFilterClick('faves')} className="film-list-filter">
+                        FAVES
+            <span className="section-count">0</span>
+                    </div>
+                </div>
+
+                {allFilms}
+            </div>
+        )
+    }
+}
+
+export default FilmListing
---
+++ src/FilmRow.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import FilmPoster from './FilmPoster'
+import Fave from './Fave'
+
+class FilmRow extends Component {
+
+    handleDetailsClick = (film) => {
+        console.log(`fetching deets for ${film}`)
+    }
+
+    render() {
+        return (
+            <div onClick={() => this.handleDetailsClick(this.props.film.title)} className='film-row'>
+                <FilmPoster film={this.props.film} />
+
+                <div className='film-summary'>
+                    <h1>{this.props.film.title}</h1>
+                    <p>{this.props.film.release_date.substring(0,4)}</p>
+                    <Fave />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FilmRow
