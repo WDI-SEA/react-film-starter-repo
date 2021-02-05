de2d32466c102bfcbeb4f4e6d7a01b085a82cbb3
Blake DeGraw <bbdegraw@gmail.com>
2021-02-04 20:36:49 -0800

    I think I'm done. Not sure. It doesn't work, but I give up.

--- src/FilmListing.js
+++ src/FilmListing.js
@@ -3,7 +3,12 @@ import FilmRow from './FilmRow'
 
 class FilmListing extends Component {
 
-    handleFilterClick = (filter) => {
+    state = {
+        filter: 'all'
+    }
+
+    handleFilterClick (filter) {
+        this.setState({filter: filter})
         console.log('Setting filter to ' + filter)
     }
 
@@ -15,11 +20,11 @@ class FilmListing extends Component {
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
                 <div className="film-list-filters">
-                    <div onClick={()=> this.handleFilterClick('all')} className="film-list-filter">
+                <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`} onClick={() => {this.handleFilterClick("all")}}>
                         ALL
                         <span className="section-count">{this.props.films.length}</span>
                     </div>
-                    <div onClick={()=> this.handleFilterClick('faves')} className="film-list-filter">
+                    <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`} onClick={()=>{this.handleFilterClick("faves")}}>
                         FAVES
                         <span className="section-count">0</span>
                     </div>
