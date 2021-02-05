562e0cb2262a407357e7e234a104cfe06ebf3fed
Blake DeGraw <bbdegraw@gmail.com>
2021-02-04 20:00:46 -0800

    starting part 5

--- src/Fave.js
+++ src/Fave.js
@@ -4,21 +4,17 @@ class Fave extends Component {
     state = {
         isFave: false
     }
-    // constructor(props){
-    //     super(props)
-    //     this.handleClick = this.handleClick.bind(this)
-    // }
+   
     handleClick = (e) => {
         e.stopPropagation()
         this.setState({isFave: !this.state.isFave})
         console.log('handling fave click!')
     }
 
-
     render() {
         return(
-            <div className="film-row-fave add_to_queue">
-                <p onClick={this.handleClick}className="material-icons">add_to_queue</p>
+            <div className={`film-row-fave ${this.state.isFave ? 'add_to_queue' : 'remove_from_queue'}`}>
+                <p innerText={`${this.state.isFave ? 'add to queue' : 'remove from queue'}`} onClick={this.handleClick}className="material-icons">add to queue</p>
             </div>
         )
     }
