590c3c71d958b65309afcd36e03a9cec9f051dbf
taekim90 <taehk15@gmail.com>
2022-03-13 21:22:12 -0700

    completion of part 3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import './App.css';
 import FilmList from './FilmList'
 import Details from './Details'
 import TMDB from './TMDB'
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="film-library">
-        <FilmList films={TMDB.films}/>
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="film-library">
+//         <FilmList films={TMDB.films}/>
 
-        <Details films={TMDB.films}/>
-      </div>
-    );
+//         <Details films={TMDB.films}/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+import React, { useState } from 'react';
+
+// functional component
+
+export default function App() {
+
+  const [TMDBInfo, setTMDB] = useState({
+    films: TMDB.films,
+    current: []
+  });
+
+  const handleDetailsClick = (film) => {
+    console.log('Fetching details for' + film.title)
+    setTMDB({
+      ...TMDBInfo, current:film
+    })
   }
-}
 
-export default App;
+  return (
+    <div className="film-library">
+      <FilmList films={TMDBInfo.films} handleDetailsClick={handleDetailsClick}/>
+
+      <Details current={TMDBInfo.current}/>
+    </div>
+  )
+}
