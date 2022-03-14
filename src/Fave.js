2fab193b187b9c4207d9123f133f23273761c51e
Norman Teodoro <normanteodoro@hotmail.com>
2022-03-14 09:39:07 -0700

    code cleanup

--- src/Fave.js
+++ src/Fave.js
@@ -3,10 +3,10 @@ export default function Fave({onFaveToggle, isFave}){
         e.stopPropagation()        
         onFaveToggle()
     }
-    const css =  isFave? 'remove_from_queue':'add_to_queue'
+    const action =  isFave? 'remove_from_queue':'add_to_queue'
     return (
 
-        <div onClick={handleClick} className={`film-row-fave ${css}`}>
+        <div onClick={handleClick} className={`film-row-fave ${action}`}>
             <p className="material-icons">add_to_queue</p>
         </div>
     )
