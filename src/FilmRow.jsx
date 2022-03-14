ec8109bb3172dbe77c23351e1ac2f0aa294bd511
mvanzo <martin.vanzo@gmail.com>
2022-03-14 11:31:19 -0700

    OMDB Part 3

--- src/FilmRow.jsx
+++ src/FilmRow.jsx
@@ -20,27 +20,3 @@ export default function FilmRow({ film, onFaveToggle, isFave, handleDetailsClick
         </div>
     )
 }
-
-
-// class FilmRow extends Component {
-//     handleDetailsClick = (film) => {
-//         console.log(`Fetching details for ${film}`)
-//     }
-
-//     render() { 
-//         let imagePath = `https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`
-//         return (
-//             <div className="film-row">
-//                 <Poster imgUrl={imagePath} alt={`Movie poster for ${this.props.film.title}`}/>
-             
-//                 <div onClick={()=>this.handleDetailsClick(this.props.film.title)} className="film-summary">
-//                     <Fave />
-//                     <h1>Title: {this.props.film.title}</h1>
-//                     <p>Year: {this.props.film.release_date.substring(0,4)}</p>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
- 
-// export default FilmRow;
