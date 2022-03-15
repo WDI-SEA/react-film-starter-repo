87a95cfd2216bf68c923778b5c891a21e4bcbe81
Jon Dimaculangan <jondmclngn@gmail.com>
2022-03-15 00:46:25 -0500

    part 4 deliverable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import FilmList from './FilmList';
 import Details from './Details';
 import { useEffect } from 'react';
@@ -18,13 +18,17 @@ const App = () => {
 
   }
 
-  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
 
   useEffect(() => {
-    axios.get(popularFilmsUrl)
-      .then(dataResponse => setFilms(dataResponse.data.results))
-
-  })
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
+    // axios.get(popularFilmsUrl)
+    //   .then(dataResponse => setFilms(dataResponse.data.results))
+    fetch(popularFilmsUrl)
+      .then(response => response.json())
+      .then(jsonData => {
+        setFilms(jsonData.results)
+      })
+  }, [])
 
   return (
     <>
