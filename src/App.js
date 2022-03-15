49dacd6102e89ed9788e4add24e1ef9ddaea8d3d
evankski <evankarwowski@gmail.com>
2022-03-15 09:30:35 -0700

    finishes part for

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import FilmList from "./FilmList";
 import Details from "./Details";
 import TMDB from "./TMDB";
-import react, { useState } from "react"
+import react, { useState, useEffect } from "react"
 
 
 export default function App() {
@@ -13,12 +13,30 @@ export default function App() {
     current: []
   });
 
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+      let data=jsonData
+      setTmdb({films:data.results, current:[]}) 
+    })
+  }, [])
+
   const handleDetailsClick = (film) => {
     console.log('Fetching details for' + film.title)
     setTmdb({
       ...tmdb, current:film
     })
   }
+
+  const url = process.env.REACT_APP_TMDB_API_KEY
+
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${url}&language=en-US&page=1`;
+
+  
+  console.log(popularFilmsUrl)
 
   return (
     <div className="film-library">
