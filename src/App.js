7c118b2d4e91a2343a1664bf066c18eaf041f46c
clairxc <clairrrc@gmail.com>
2022-03-14 17:57:04 -0700

    part 4 complete

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import FilmList from './FilmList';
 import Details from './Details';
 import TMDB from './TMDB'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 // class App extends Component {
@@ -22,9 +22,7 @@ import React, { useState } from 'react'
 // refactor to functional component
 export default function App() {
 
-  const[films, setFilms] = useState({
-    allFilms: TMDB.films
-  })
+  const[films, setFilms] = useState([])
 
   const [currentFilm, setCurrentFilm] = useState({})
 
@@ -33,10 +31,25 @@ export default function App() {
     setCurrentFilm(film)
   }
 
+  
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    // console.log(popularFilmsUrl)
+    console.log("UseEffect is firing!")
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+      setFilms(jsonData.results)
+      setCurrentFilm(currentFilm)
+    })
+    .catch(error=>console.log('error fetching data'))
+  }, [])
+
   return (
     <div className="film-library">
-      <FilmList handleDetailsClick={handleDetailsClick} setFilms={films} films={TMDB.films} />
-      <Details currentFilm={currentFilm} films={TMDB.films}/>
+      <FilmList handleDetailsClick={handleDetailsClick} setFilms={films} films={films} />
+      <Details currentFilm={currentFilm} films={films}/>
     </div>
   )
 }
