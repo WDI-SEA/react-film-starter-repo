4ee3b628f9b914394f1f1e445578a4f36b5684a9
Sol Youn <syoun93@gmail.com>
2022-03-14 17:18:43 -0700

    OMDB part 4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import FilmList from "./FilmList";
 import Details from "./Details";
 import TMDB from "./TMDB"
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function App () {
 
-  const [films] = useState(TMDB.films)
+  const [films, setFilms] = useState([])
   const [current, setCurrent] = useState([])
 
   const handleDetailsClick = (film) => {
     console.log(`Fetching details for ${film.title}`)
     setCurrent(film)
   }
+
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    console.log("UseEffect is firing!")
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+      setFilms(jsonData.results)
+    })
+
+  }, [])
 
   return (
     <div className='App'>
