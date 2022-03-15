14f37291ec37cbdedb2a128323b1d6d1721a7919
mvanzo <martin.vanzo@gmail.com>
2022-03-15 10:06:06 -0700

    TMBD part 4 complete

--- src/App.js
+++ src/App.js
@@ -2,16 +2,37 @@ import './App.css';
 import FilmList from './FilmList';
 import Details from './Details';
 import TMDB from './TMDB';
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios';
 
 export default function App() {
-  // state - films
-  const [films, setFilms] = useState({
-    allFilms: TMDB.films
-  })
-
   // state - current film
   const [currentFilm, setCurrent] = useState('')
+
+  // state - films
+  const [films, setFilms] = useState([])
+
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+
+  // fetch .then
+  // useEffect(() => {
+  //   fetch(popularFilmsUrl)
+  //   .then(response => response.json())
+  //   .then(jsonData => {
+  //     // const movieArr = Object.values(jsonData.data)
+  //     // console.log(jsonData.results)
+  //     setFilms(jsonData.results)
+  //   })
+  // }, [])
+
+  // fetch async/await
+  useEffect(()=> {
+    (async ()=> {
+      const response = await fetch(popularFilmsUrl)
+      const json = await response.json()
+      setFilms(json.results)
+    })()
+  }, [])
 
   const handleDetailsClick = (film) => {
     console.log(`Fetching details for ${film}`)
@@ -20,8 +41,8 @@ export default function App() {
 
   return(
     <div className="film-library">
-      <FilmList handleDetailsClick={handleDetailsClick} stateFilms={films} films={TMDB.films} />
-      <Details currentFilm={currentFilm} films={TMDB.films} />
+      <FilmList handleDetailsClick={handleDetailsClick} films={films} />
+      <Details currentFilm={currentFilm} films={films} />
     </div>  
   )
 }
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,4 +1,3 @@
-
 export default function Details(props) {
 
     const filmInfo = (
