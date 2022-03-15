015ea241514eeb90e3a4f219193c9dc73bed792c
taekim90 <taehk15@gmail.com>
2022-03-14 17:18:26 -0700

    completion of part 4

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import FilmList from './FilmList'
 import Details from './Details'
 import TMDB from './TMDB'
+import React, { useState, useEffect } from 'react';
 
 
 // class App extends Component {
@@ -20,12 +21,11 @@ import TMDB from './TMDB'
 // export default App;
 
 
-import React, { useState } from 'react';
 
 // functional component
 
 export default function App() {
-
+  console.log(TMDB.api_key)
   const [TMDBInfo, setTMDB] = useState({
     films: TMDB.films,
     current: []
@@ -37,6 +37,21 @@ export default function App() {
       ...TMDBInfo, current:film
     })
   }
+  
+  // const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+  // console.log((popularFilmsUrl))
+  
+  useEffect(() => {
+    console.log("UseEffect is firing!")
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+      setTMDB({ ...TMDBInfo, films: jsonData.results})
+    })
+  }, [])
+
 
   return (
     <div className="film-library">
