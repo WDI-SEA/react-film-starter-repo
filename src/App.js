43e94c2bb454396eec0dc52ed9f61510eb8f94de
“ikarabag1” <irmakkarabag@gmail.com>
2022-03-15 19:59:33 +0300

    PART 4 complete - api calls 4 ways request-code along added as well

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, {useState} from 'react'
+import axios from 'axios';
+import React, {useState, useEffect} from 'react'
 import Details from './Details';
 import FilmList from "./FilmList";
 import TMDB from './TMDB'
+
+const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
 
 // FUNCTION IT UP
 // PART 3
@@ -13,6 +16,35 @@ export default function App() {
   const [films, setFilms] = useState(TMDB.films);
   // passing data to empty array for a film
   const [current, setCurrent] = useState({})
+  
+  // MVP FOR PART 4 DELVIERABLE --API CALLS 4 WAYS
+  // TEST IF IT IS CONSOLOING OUT ON CONSOLE BRWOSER DEV TOOL
+// console.log(process.env.REACT_APP_API_KEY, process.env.REACT_APP_SERVER_URL)
+useEffect(()=> {
+
+  // FETCH DOT THEN --this is in read me
+// fetch(popularFilmsUrl)
+// .then(res => res.json())
+// .then(json => setFilms(json.results))
+
+  // FETCH ASYNC/AWAIT IIFE
+  // (async() => { //wait for fetch the res json parameter, the set films to be json results
+  //   const res = await fetch(popularFilmsUrl)
+  //   const json = await res.json()
+  //   setFilms(json.results)
+  // })
+
+  // AXIOS.then --gives us the response and always has data in it : we need to response.data.results
+  // axios.get(popularFilmsUrl)
+  // .then(response => setFilms(response.data.results))
+
+  // AXIOS ASYNC/AWAIT
+  (async () => {
+    const response = await axios.get(popularFilmsUrl)
+    setFilms(response.data.results)
+  })()
+}, [])
+
 
   // TASK 3 -- MOVE DETAILS EVENT HANDLER TO HERE UP COMPONENT TREE
 // move the handleDetailsClick to show details of mvie from filmrow to here, since this is the aprent of both filmlist and details
