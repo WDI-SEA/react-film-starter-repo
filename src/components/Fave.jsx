3b70794cac9fc22bd7cd457ea2decf2c2ddce20f
mitchdmarino <mitchdmarino@gmail.com>
2022-06-13 22:31:49 -0700

    finishes pt 3

---
+++ src/components/Fave.jsx
@@ -0,0 +1,24 @@
+import  { useState } from 'react'
+
+
+
+export default function Fave (props) {
+   
+    const handleClick = e => {
+        e.stopPropagation()
+        console.log('Handling Fave click!')
+        // Add this line. You'll call the function passed through props
+        props.onFaveToggle()
+        // Delete the `setIsFaves` line. You no longer track state here
+        // setIsFave(!isFave)
+      }
+    
+    const action = props.isFave?'remove_from_queue':'add_to_queue'
+    return (
+        <div className={`film-row-fave ${action}`}
+        onClick={handleClick}>
+            <p className="material-icons">add_to_queue</p>
+        </div>
+    )
+    
+}
