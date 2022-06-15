8225d4b73eee19f88df2abf1d8d38e734e36ab45
carlos-villatoro <futurebound.carlosv@gmail.com>
2022-06-14 23:30:52 -0500

    finishes part 4, uses api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 import FilmList from './component/FilmList'
 import Detail from './component/Detail';
 import TMDB from './TMDB'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  const films = TMDB.films
+  const [films, setFilms] = useState([])
   const [current, setCurrent] = useState({})
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`;
+  useEffect(() => {
+    fetch(popularFilmsUrl)
+      .then(response => response.json())
+      .then(jsonData => {
+        setFilms(jsonData.results)
+      })
+  }, [popularFilmsUrl])
   const handleDetailsClick = (film) => {
     setCurrent(film)
   }
