ce08f09a6fbd8a4bc0c688512b3bfe74296365fc
ASelviyan <angelikaselviyan5@gmail.com>
2022-06-14 18:21:10 -0600

    mvp completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import FilmList from './components/FilmList';
 import Details from './components/Details';
 import TMDB from './TMDB'
 
 export default function App() {
-  const [films, setFilms] = useState(TMDB.films)
+  const [films, setFilms] = useState([])
   const [current, setCurrent] = useState({})
+
+
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    fetch(popularFilmsUrl)
+      .then(response => response.json())
+      .then(jsonDeets => {
+        console.log(jsonDeets)
+        setFilms(jsonDeets.results)
+      })
+  }, [])
+
 
   //handlers
   const handleDetailsClick = (film) => {
