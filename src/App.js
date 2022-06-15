2ef76bbaef42ac86f58a45ffd249c8bb9edffd51
gracenarez333 <gracemnarez@gmail.com>
2022-06-14 17:25:01 -0700

    finishes part 4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import './App.css';
 import FilmList from './components/FilmList';
 import Details from './components/Details';
 import TMDB from './TMDB'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  const films = TMDB.films
+  const [films, setFilms] = useState([])
   const [current, setCurrent] = useState({})
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+  useEffect(() => {
+    fetch(popularFilmsUrl)
+      .then(response => response.json())
+      .then(jsonData => {
+        setFilms(jsonData.results)
+      })
+  }, [popularFilmsUrl])
   const handleDetailsClick = (film) => {
     setCurrent(film)
   }
