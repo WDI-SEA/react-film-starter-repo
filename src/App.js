981248c7e41ddc59fadc7571b42d87646bc8c7a6
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-06-14 18:02:57 -0600

    Complete deliverable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import "./App.css"
 import FilmList from "./components/FilmList"
 import Details from "./components/Details"
-import movies from "./TMDB"
-import { useState } from "react"
+import TMDB from "./TMDB"
+import { useState, useEffect } from "react"
 
 const App = () => {
   const [selectedFilm, setSelectedFilm] = useState({})
-  const [films, setFilms] = useState(movies.films)
+  const [films, setFilms] = useState(TMDB.films)
+
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`
+
+  useEffect(() => {
+    console.log("UseEffect is firing!")
+    fetch(popularFilmsUrl)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setFilms(jsonData.results)
+      })
+  }, [])
 
   const handleDetailsClick = (film) => {
     console.log(`Fetching details for ${film.title}`)
