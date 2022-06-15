d10a6e4f6d0f1b28ee6968ab9643dd7682842c53
Hoan-K-Le <hoanleecr@gmail.com>
2022-06-14 17:40:27 -0700

    finished part4deliverable

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import FilmList from './components/FilmList';
 import Details from './components/Details';
 // import movies from './TMDB'
-import { useState } from 'react'
 import TMDB from './TMDB'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 // import React from 'react'
 
 
@@ -13,8 +14,26 @@ import TMDB from './TMDB'
 const App = () => {
   const [current, setCurrent] = useState({})
   const [films, setFilms] = useState(TMDB.films)  
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`
 
-  const handleDetailsClick = (film) => {
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await axios.get(popularFilmsUrl)
+        // console.log(popularFilmsUrl)
+        // const res = await axios.get(url)
+        res = res.data
+        setFilms(res.data.results)
+
+      } catch(err) {
+        console.log('err', err)
+      }
+    })()
+  }, [popularFilmsUrl])
+
+
+  const handleDetailsClick = async film => {
     console.log(`Fetching details for ${film.title}`)
     setCurrent(film)
 }
