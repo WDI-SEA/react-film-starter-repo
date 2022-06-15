df40db6420d009f88884725e93d47e0b83acd265
“erhaneth” <gumusucb21@gmail.com>
2022-06-14 22:23:15 -0700

    Finished deliverable

--- src/App.js
+++ src/App.js
@@ -4,44 +4,33 @@ import Details from './components/Details';
 import FilmList from './components/FilmList';
 import TMDB from './TMDB'
 import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
   const [current, setCurrent] = useState({})
-  const [films] = useState(TMDB.films)
+  const [films, setFilms] = useState(TMDB.films)
+  // const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+  // // console.log(popularFilmsUrl)
+  // useEffect(() => {
+  //   // IIFE immediately invoked function expression
+  //   // the IIFE is to avoid putting async on the useEffect callback
+  //   (async () => {
+  //     try {
+  //       const res = await axios.get(popularFilmsUrl)
+  //       console.log(res)
+  //       setFilms(res.data.results)
+  //     } catch (err) {
+  //       console.warn(err)
+  //     }
+  //   })()
   
-
+  // }, [])
+ 
   const handleDetailsClick = (film) => {
     console.log("Fetching details for", film.title)
     setCurrent(film)
   }
-  useEffect(() => {
-		// IIFE immediately invoked function expression
-		// the IIFE is to avoid putting async on the useEffect callback
-		(async () => {
-			try {
-				const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
-				const jsonData = await popularFilmsUrl.json()
-				// setRepos(response.data)
-				console.log(jsonData)
-			} catch (err) {
-				console.warn(err)
-			}
-		})()
-		// console.log('?')
-	}, [])
-  // useEffect(() => {
-  //   // fetch data
-  //   const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
-  //   const jsonData = await popularFilmsUrl.json()
-  //   console.log(jsonData)
-  //   // fetch(popularFilmsUrl)
-  //     // .then(res => res.json())
-  //     // .then(wisdom => {
-  //     //   // set it in state
-  //     //   console.log(wisdom)
-  //     //   setWiseWords(wisdom.quote)
-  //     // })
-  // }, [])
+
   return (
     <div className="film-library">
       <FilmList
