5a3d957687e6b0b8bf0a6baff5e30b1fd5272532
“j-onederfulgit config --global user.email “jackwwyman@gmail.com <jackwyman@macbook-pro-2.lan>
2022-06-14 17:40:34 -0700

    finished part 4 right

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css'
 import FilmList from './components/FilmList'
 import Details from './components/Details'
 import TMDB from './TMDB'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 const App = () => {
+  const [films, setFilms] = useState([])
   const [current, setCurrent] = useState({})
   
-  const films = TMDB.films
+  // const films = TMDB.films
+
+  // const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`; 
+
+  // useEffect(() => {
+  //   console.log("UseEffect is firing!")
+  //   fetch(popularFilmsUrl)
+  //   .then(response => response.json())
+  //   .then(jsonData => {
+  //     console.log(jsonData)
+  //   })
+  // }, [])
+
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonDeets => {
+      setFilms(jsonDeets.results)
+    })
+  }, [])
 
   const handleDetailsClick = film => {
     setCurrent(film)
