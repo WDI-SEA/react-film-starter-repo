14ccc1bb449443bd4885f4c699661193c7fdb3ff
marzo20 <myjune91@gmail.com>
2022-06-15 09:36:25 -0700

    part4 done.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import { useState } from 'react'
+import {
+  useState,
+  useEffect
+} from 'react'
 import './App.css';
 import FilmList from './components/FilmList'
 import Details from './components/Details'
 import TMDB from './TMDB';
 
-function App () {
-  const films = TMDB.films
+function App() {
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
+  const [films, setFilms] = useState([])
+  useEffect(()=>{
+    console.log("useEffect is firing!",popularFilmsUrl)
+    fetch(popularFilmsUrl)
+      .then(response => response.json())
+      .then(jsonData => {
+        console.log(jsonData)
+        setFilms(jsonData.results)
+      })
+      
+  },[])
   const [current, setCurrent] = useState({})
   function handleDetailsClick(e) {
-    console.log('fetching details for', e )
-     setCurrent(e)
-}
-    return (
-      <div className="film-library">
-        <div className="film-list">
-          <h1 className="section-title">FILMs</h1>
-          <FilmList 
-          films={films}
-          handleDetailsClick={handleDetailsClick}/>
-        </div>
-
-        <div className="film-details">
-          <h1 className="section-title">DETAIL
-          </h1>
-          <Details 
-              film={current} />
-        </div>
-      </div>
-    );
+    console.log('fetching details for', e)
+    setCurrent(e)
   }
+
+  return (
+    <div className="film-library">
+      <div className="film-list">
+        <h1 className="section-title">FILMs</h1>
+        <FilmList
+          films={films}
+          handleDetailsClick={handleDetailsClick} />
+      </div>
+
+      <div className="film-details">
+        <h1 className="section-title">DETAIL
+        </h1>
+        <Details
+          film={current} />
+      </div>
+    </div>
+  );
+}
 
 
 export default App;
