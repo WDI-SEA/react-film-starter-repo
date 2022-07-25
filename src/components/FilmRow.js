d95686d15654338c6c9760a9a199ca9cf342545e
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-25 19:24:29 -0400

    Each film row now has the movie picture, the title of the move and year it was relased.

--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -4,7 +4,11 @@ export default class FilmRow extends Component {
      render() {
           return (
                <div className="film-row">
-                    <h1 key={this.props.key}>{this.props.film.title}</h1>
+                    <img src={"https://image.tmdb.org/t/p/w780"+ this.props.film.poster_path} alt="{this.props.film.title}" />
+                    <div className="film-summary">
+                         <h1>{this.props.film.title}</h1>
+                         <p>{this.props.film.release_date}</p>
+                    </div>
                </div>
           )
      }
