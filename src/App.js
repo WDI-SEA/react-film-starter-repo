a2a1ba4d2bad7e38598ee66b739091d3617dcbc7
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-25 16:44:28 -0400

    All films show up in the FILMS list.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import FilmList from './components/FilmList'
 import Details from './components/Details'
+import filmsDB from './TMDB'
 
 class App extends Component {
   render() {
     return (
-      
       <div className="film-library">
-        <FilmList />
-        <Details />
+        <FilmList films={filmsDB.films} />
+        <Details films={filmsDB.films} />
 </div>
     );
   }
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 export default class FilmList extends Component {
+     print = () => console.log(this.props.films[0].title)
+     const 
      render() {
+          const allFilms = this.props.films.map((film, key) => {
+                return (<div className="film-row">
+                              <h1 key={key}>{film.title}</h1>
+                         </div>)
+          })
           return (
                <div className="film-list">
                     <h1 className="section-title">FILMS</h1>
+                    {allFilms}
                </div>
           )
      }
