ada3fc4a70649eff1859248dd148586a484915f2
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-25 19:37:30 -0400

    Moved the img tag inot it's own component.

--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react'
+import Poster from './Poster'
 
 export default class FilmRow extends Component {
      render() {
           return (
                <div className="film-row">
-                    <img src={"https://image.tmdb.org/t/p/w780"+ this.props.film.poster_path} alt="{this.props.film.title}" />
+                    <Poster src={"https://image.tmdb.org/t/p/w780"+ this.props.film.poster_path} alt={this.props.film.title} />
                     <div className="film-summary">
                          <h1>{this.props.film.title}</h1>
                          <p>{this.props.film.release_date}</p>
