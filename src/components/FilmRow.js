748caa44966d3bf31b34d93f4dbe19e4663287bf
jlx012 <james.lee01230@gmail.com>
2022-07-25 15:56:03 -0500

    Edited and created Poster component

--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Poster from './Poster'
 
 class FilmRow extends Component {
     render() {
         const date = new Date(this.props.film.release_date).getFullYear()
         return (
             <div className="film-row">
-                <img src={`https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`} />
+                <Poster film={this.props.film}/>
 
                 <div className="film-summary">
                     <h1>{this.props.film.title}</h1>
