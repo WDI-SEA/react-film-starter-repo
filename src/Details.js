61affcb041f7946c7321cbf9377c94160cdf1023
danstacy1 <danstacy1@gmail.com>
2022-07-25 16:47:56 -0400

    completed

---
+++ src/Details.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react' 
+
+class Details extends Component {
+    // we want to pass down the filterValue
+    // we also want to pass down the handleFilterChange
+    render() {
+        return (
+            <>
+                <div className="film-details">
+                    <h1 className="section-title">DETAILS</h1>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Details
---
+++ src/FilmList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react' 
+import FilmRow from './FilmRow'
+
+class FilmList extends Component {
+    // we want to pass down the filterValue
+    // we also want to pass down the handleFilterChange
+    render() {
+        const allFilms = this.props.films.map((film, index) => {
+            return (
+                <FilmRow 
+                    film={film}
+                    key={index}/>
+            )
+        })
+
+        return (
+            <>
+                <div className="film-list">
+                    <h1 className="section-title">FILMS</h1>
+                    {/* <h1>{this.props.films[0].title}</h1> */}
+                    {allFilms}
+                </div>
+            </>
+        )
+    }
+}
+
+export default FilmList
