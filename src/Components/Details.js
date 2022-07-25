cc970b877097cfcdd03fddb661b6564032bc022c
christian-the-brewer <brewer.christiang@gmail.com>
2022-07-25 15:41:27 -0400

    Adds components to app.js

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -4,7 +4,10 @@ class Details extends Component {
 
     render() {
         return (
-            
+            <div className="film-details">
+                <h1 className="section-title">DETAILS</h1>
+            </div>
+
         )
     }
 }
--- src/Components/FilmList.js
+++ src/Components/FilmList.js
@@ -1,5 +1,18 @@
 import React, { Component } from 'react'
 
+class FilmList extends Component {
 
+    render() {
+        return (
+
+            <div className="film-library">
+                <div className="film-list">
+                    <h1 className="section-title">FILMS</h1>
+                </div>
+            </div>
+
+        )
+    }
+}
 
 export default FilmList
