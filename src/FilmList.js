c98b8c529fbbbf0ddcbb4446c6782c380458faa3
Alys <alice.g.cooper@gmail.com>
2022-07-25 14:51:15 -0400

    complete step 8

--- src/FilmList.js
+++ src/FilmList.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import FilmRow from './FilmRow';
 
 class FilmList extends Component {
     render() {
+        const allFilms = this.props.films.map((film,idx) => {
+            return <FilmRow film={film}/>
+            // <h1>{film.title}</h1>
+        })
         return (
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
+                {allFilms}
             </div>
         );
     }
