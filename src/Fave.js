3baf9bb3a976211521ca9aa4091f8e03eed9094b
nrissman <nrissman90@gmail.com>
2022-07-26 19:59:46 -0400

    part two complete

--- src/Fave.js
+++ src/Fave.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 
 class Fave extends Component {
-
+    state={
+        isFave: 'false'   
+    } 
     
     render(){
         const handleClick = (e) => {
-            e.preventDefault()
+            e.stopPropagation()
             console.log('handling Fave click')
+            this.setState((prevState) =>{
+                return{
+                    isFave: !prevState.isFave
+                }
+            })
         }
 
 
         return( 
             <>
-            <div className="film-row-fave add_to_queue"
+            <div className={`film-row-fave ${this.state.isFave === true ? 'remove_from_queue' : 'add_to_queue'}`} 
                 onClick={handleClick}>
-                <p className="material-icons">add_to_queue</p>
+                <p className="material-icons">
+                {this.state.isFave === true ? 'remove_from_queue' : 'add_to_queue'}
+                </p>
             </div>
             </>
         )
--- src/FilmRow.js
+++ src/FilmRow.js
@@ -6,12 +6,15 @@ import Fave from './Fave'
 class FilmRow extends Component {
     // we want to pass down the filterValue
     // we also want to pass down the handleFilterChange
+    handlesDetailsClick = (film) => {
+        console.log('fetching details for', this.props.film.title)
+    }
     render() {
         const posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`
         const date = new Date (this.props.film.release_date).getFullYear()
         
         return (
-            <div className="film-row">
+            <div className="film-row" onClick={this.handlesDetailsClick}>
                 <Poster posterUrl={posterUrl} alt={this.props.title}/>
 
                 <div className="film-summary">
