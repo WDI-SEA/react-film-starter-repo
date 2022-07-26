a24aefbac0e5ce8c1bc6cc285edff47a13b0ddb6
RandyD <rdsoundguy@gmai.com>
2022-07-26 14:42:22 -0400

    Step 2 complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Details from './Details';
+import FilmList from './FilmList';
 
 class App extends Component {
   render() {
     return (
+      <>
 
-      // haven't had a change to work on this yet
-    );
+      <FilmList />
+      <Details />      
+      
+      </>
+
+
+  
+    )
   }
 }
 
---
+++ src/FilmList.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+
+class FilmList extends Component {
+
+    render() {
+
+        return (
+    <>
+
+    <div className="film-library">
+        <div className="film-list">
+            <h1 className="section-title">FILMS</h1>
+        </div>
+    </div>
+
+    
+    </>
+
+
+        )
+    }
+}
+
+export default FilmList;
