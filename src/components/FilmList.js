9c09be4db60dd605857b18d176cb47ab7ade05a8
DRG104 <ph_kyle104@yahoo.com>
2022-07-26 13:44:48 -0500

    Part 2 OMDB complete

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -2,16 +2,46 @@ import React, { Component } from 'react'
 import FilmRow from './FilmRow'
 
 class FilmList extends Component {
+
+    state = {
+        filter: "all"
+    }
+
+    handleFilterClick = (e, filter)  => {
+        console.log("an filter was clicked" + {filter})
+        this.setState({
+            filter: e
+        })
+    }
     render () {
-        const allFilms = this.props.films.map((idx) => {
+        const allFilms = this.props.films.map((film, idx) => {
             return (
-                <FilmRow key={idx} film={idx} />
+                <FilmRow key={idx} film={film} />
             )
         })
         return (
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
-                <h1>{allFilms}</h1>
+                <div className="film-list-filters">
+                    <div 
+                        className={
+                            `film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`} 
+                            onClick={() => {this.handleFilterClick("all")}}
+                        >
+                        ALL
+                        <span className="section-count">{this.props.films.length}</span>
+                    </div>
+                    <div 
+                        className={
+                            `film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`}
+                            onClick={ () => this.handleFilterClick('faves')}
+                        >
+                            FAVES
+                        <span className="section-count">0</span>
+                    </div>
+                </div>
+
+                {allFilms}
             </div>
         )
     }
--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
 import Poster from './Poster'
+import Fave from './Fave'
 
 class FilmRow extends Component {
+
+    handleDetailsClick = (film)  => {
+        console.log(`printing details for ${film}`)
+        // this.setState({
+        //     filter: e
+        // })
+    }
+
     render () {
         const posterUrl = "https://image.tmdb.org/t/p/w780/" + this.props.film.poster_path
         const year = this.props.film.release_date.substring(0, 4)
 
         return (
-            <div className="film-row">
+            <div 
+                className="film-row"
+                onClick={ () => {this.handleDetailsClick(this.props.film.title)}}
+                >
                 <Poster 
                     url={posterUrl}
                     title={this.props.film.title}
@@ -17,6 +29,8 @@ class FilmRow extends Component {
                     <h1>{this.props.film.title}</h1>
                     <p>{year}</p>
                 </div>
+
+                <Fave />
             </div>
         )
     }
