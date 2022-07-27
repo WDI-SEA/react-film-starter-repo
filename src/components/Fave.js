62abafe845fd070ba094564c027901a77f491e63
cjones1047 <cjmj1047@gmail.com>
2022-07-27 08:10:13 -0500

    Part 2, Goal 4 completed

--- src/components/Fave.js
+++ src/components/Fave.js
@@ -2,18 +2,25 @@ import React , { Component } from 'react'
 
 class Fave extends Component {
 
+    state = {
+        isFave: false
+    }
+
     handleClick = e => {
         e.preventDefault()
-        console.log('handling Fave click!')
+        this.setState((prevState)=>{ 
+            return {isFave: !prevState.isFave} 
+        })
         e.stopPropagation()
     }
 
     render() {
+        const action = this.state.isFave?'remove_from_queue':'add_to_queue'
         return (
-            <div className="film-row-fave add_to_queue"
+            <div className={`film-row-fave ${action}`}
                 onClick={this.handleClick}
             >
-                <p className="material-icons">add_to_queue</p>
+                <p className="material-icons">{action}</p>
             </div>
         );
     }
