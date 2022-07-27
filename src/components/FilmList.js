15e0e86974241b9eb5d6dc350498663dbebc3bee
nickesparza <nickesparza@gmail.com>
2022-07-27 08:31:44 -0500

    finish omdb part 2

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react'
 import FilmRow from './FilmRow'
 
 class FilmList extends Component {
+
+    state = {
+        filter: 'all'
+    }
+
+    handleFilterClick = (filter) => {
+        // console.log(`a filter was clicked, it was ${filter}`)
+        this.setState(() => {
+            return {
+                filter: filter
+            }
+        })
+    }
+
     render() {
         const allFilms = this.props.films.map((film, index) => {
             return <FilmRow film={film} key={index}/>
@@ -9,6 +23,16 @@ class FilmList extends Component {
         return(
             <div className="film-list">
                 <h1 className="section-title">FILMS</h1>
+                <div className="film-list-filters">
+                    <div className={`film-list-filter ${this.state.filter === 'all' ? 'is-active' : ''}`} onClick={() => this.handleFilterClick('all')}>
+                        ALL
+                        <span className='section-count'>{this.props.films.length}</span>
+                    </div>
+                    <div className={`film-list-filter ${this.state.filter === 'faves' ? 'is-active' : ''}`} onClick={() => this.handleFilterClick('faves')}>
+                        FAVES
+                        <span className='section-count'>0</span>
+                    </div>
+                </div>
                 {allFilms}
             </div>
         )
--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 import Poster from './Poster'
+import Fave from './Fave'
 
 class FilmRow extends Component {
+
+    handleDetailsClick = (film) => {
+        console.log(`fetching details for ${this.props.film.title}`)
+    }
+
     render() {
         const posterUrl = `https://image.tmdb.org/t/p/w780/${this.props.film.poster_path}`
         const year = this.props.film.release_date.substring(0, 4)
         return(
-            <div className='film-row'>
+            <div className='film-row' onClick={(film) => this.handleDetailsClick(film)}>
                 <Poster posterUrl={posterUrl} alt={this.props.film.title}/>
                 <div className='film-summary'>
                     <h1>{this.props.film.title}</h1>
                     <p>{year}</p>
                 </div>
+                <Fave/>
             </div>
         )
     }
