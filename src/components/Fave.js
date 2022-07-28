6132c5d7f0f5485dc526fdd548cc188354853996
jlx012 <james.lee01230@gmail.com>
2022-07-28 00:54:40 -0500

    OMDB Films Part 2 Complete

--- src/components/Fave.js
+++ src/components/Fave.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class Fave extends Component {
+    state = {
+        isFave: false
+    }
+    handleClick = (e) => {
+        console.log('Handling Fave click!')
+
+        e.stopPropagation()
+        this.setState((prevState) => {
+            return {
+                isFave: !prevState.isFave
+            }
+        })
+    }
+
     render() {
-        const handleClick = (e) => {
-            e.preventDefault()
-            console.log('Handling Fave click!')
-        }
 
         return (
-            <div className="film-row-fave add_to_queue" onClick={handleClick}>
+            <div className={`film-row-fave ${this.state.isFave ? 'remove_from_queue' : 'add_to_queue'}`} onClick={this.handleClick}>
                 <p className="material-icons">add_to_queue</p>
             </div>
         )
--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -3,10 +3,15 @@ import Poster from './Poster'
 import Fave from './Fave';
 
 class FilmRow extends Component {
+    handleDetailsClick = (film) => {
+        console.log('Fetching details for', this.props.film.title )
+    }
+    
     render() {
         const date = new Date(this.props.film.release_date).getFullYear()
         return (
-            <div className="film-row">
+            <div className="film-row" onClick={this.handleDetailsClick}>
+                
                 <Poster film={this.props.film}/>
 
                 <div className="film-summary">
