b0b0b0305e4709f55fea91c137664232b66f01c0
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-29 08:26:16 -0400

    Make the FilmRows clickable

--- src/components/FilmRow.js
+++ src/components/FilmRow.js
@@ -3,11 +3,14 @@ import Poster from './Poster'
 import Fave from './Fave'
 
 export default class FilmRow extends Component {
+     handleDetailsClick = () => {
+          console.log("Title: ", this.props.film.title)
+     }
      render() {
           return (
                <div className="film-row">
                     <Poster src={"https://image.tmdb.org/t/p/w780"+ this.props.film.poster_path} alt={this.props.film.title} />
-                    <div className="film-summary">
+                    <div className="film-summary" onClick={this.handleDetailsClick}>
                          <h1>{this.props.film.title}</h1>
                          <p>{this.props.film.release_date}</p>
                     </div>
