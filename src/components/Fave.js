33e3b6a054eeb5049df8698e596727638b544998
efrainadavila@gmail.com <efraindavila@Efrains-MacBook-Pro.local>
2022-07-29 08:31:25 -0400

    put a comment for Stop the event propagation because my app does not appear to need it. So I do not understand what this is doing. Maybe i missed a step.

--- src/components/Fave.js
+++ src/components/Fave.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 export default class Fave extends Component {
      render() {
-          const handleClick = e => {console.log("handling Fave click!")}
+          const handleClick = e => {console.log("handling Fave click!")} //e.stopPropagation() // Says I need this to only see one message. I already am. PUt maybe i missed soemthing so I will keep this here so I can easlly add it.
           return (
                <div className="film-row-fave add_to_queue" onClick={handleClick}>
                     <p className="material-icons">add_to_queue</p>
