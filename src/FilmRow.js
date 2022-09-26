b9c792aa03cdcb2870059dd358f26dd87fb02150
weston-bailey <wdbailey87@gmail.com>
2022-09-26 15:32:16 -0700

    finishes part 1

---
+++ src/FilmRow.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Poster from './Poster'
+
+export default class FilmRow extends Component {
+    render() {
+        return (
+            <div className='film-row'>
+                <Poster 
+                    poster_path={this.props.film.poster_path}
+                    title={this.props.film.title}
+                />
+
+                <div className='film-summary'>
+                    <h1>{this.props.film.title}</h1>
+
+                    {/* <p>{this.props.film.release_date.split('-')[0]}</p> */}
+                    {/* <p>{new Date(this.props.film.release_date).getFullYear()}</p> */}
+                    <p>{this.props.film.release_date.substring(0, 4)}</p>
+                </div>
+            </div>
+        )
+    }
+}
