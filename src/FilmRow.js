9a3233a1a81c801504626fdb31843106b0002f04
Agrate071818 <zionagrate@gmail.com>
2022-09-28 19:10:05 -0400

    Before Function refactor

---
+++ src/FilmRow.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Poster from './Poster'
+import Fave from './Fave'
+
+export default class FilmRow extends Component {
+
+    handleDetailsClick = film => {
+        console.log('fetching details for film:', film.title)
+    }
+
+    render() {
+        return (
+            <div className='film-row'
+                onClick={() => this.handleDetailsClick(this.props.film)}
+            >
+                <Poster 
+                    poster_path={this.props.film.poster_path}
+                    title={this.props.film.title}
+                />
+
+                <div className='film-summary'>
+                    <h1>{this.props.film.title}</h1>
+
+                    {/* <p>{this.props.film.release_date.split('-')[0]}</p> */}
+                    {/* <p>{new Date(this.props.film.release_date).getFullYear()}</p> */}
+                    <p>{this.props.film.release_date.substring(0, 4)}</p>
+                </div>
+
+                <Fave />
+            </div>
+        )
+    }
+}
