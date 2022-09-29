42300afee6cac477e109f192ee4fb05a753464ad
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 19:39:59 -0500

    Refactor FilmRow

--- src/FilmRow.jsx
+++ src/FilmRow.jsx
@@ -1,25 +1,49 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 import Fave from './Fave'
 // import Poster from './Poster'
 
-export default class FilmRow extends Component {
 
-    handleDetailsClick = (film) => {
+export default function FilmRow(props) {
+
+    const handleDetailsClick = (film) => {
         console.log('Fetching details for', film.title)
     }
 
-    render() {
-        return (
-            <div className='film-row' onClick={() => this.handleDetailsClick(this.props.film)}>
-                <img src={'https://image.tmdb.org/t/p/w780/'+ this.props.film.poster_path} alt={this.props.film.title} />
+    return (
+        <div className='film-row' onClick={() => handleDetailsClick(props.film)}>
+            <img src={'https://image.tmdb.org/t/p/w780/'+ props.film.poster_path} alt={props.film.title} />
 
-                <div className="film-summary">
-                    <h1>Title: {this.props.film.title}</h1>
-                    <p>Year: {this.props.film.release_date.split('-')[0]}</p>
-                </div>
-
-                <Fave />
+            <div className="film-summary">
+                <h1>Title: {props.film.title}</h1>
+                <p>Year: {props.film.release_date.split('-')[0]}</p>
             </div>
-        )
-    }
+
+            <Fave />
+        </div>        
+    )
 }
+
+
+
+
+// export default class FilmRow extends Component {
+
+    // handleDetailsClick = (film) => {
+    //     console.log('Fetching details for', film.title)
+    // }
+
+//     render() {
+//         return (
+//             <div className='film-row' onClick={() => this.handleDetailsClick(this.props.film)}>
+//                 <img src={'https://image.tmdb.org/t/p/w780/'+ this.props.film.poster_path} alt={this.props.film.title} />
+
+//                 <div className="film-summary">
+//                     <h1>Title: {this.props.film.title}</h1>
+//                     <p>Year: {this.props.film.release_date.split('-')[0]}</p>
+//                 </div>
+
+//                 <Fave />
+//             </div>
+//         )
+//     }
+// }
