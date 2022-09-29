6fd260fa703390496de252c9b24c2b43920fa353
brandon-w1205 <bwu1205@gmail.com>
2022-09-29 16:53:28 -0700

    adds TMDB functionality, completes deliverable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import FilmList from './FilmList'
 import Details from './Details'
 import TMDB from './TMDB'
 
 export default function App() {
-  const [films] = useState(TMDB.films)
+  const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrentState] = useState([])
+
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    console.log("UseEffect is firing!")
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+      setFilms(jsonData.results)
+    })
+  }, [])
 
   const handleDetailsClick = (film) => {
     console.log(`Fetching details for ${film}`)
