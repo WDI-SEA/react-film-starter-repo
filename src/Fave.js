0d2eebfd047efa1ce3da5cad5674fe1545c0e5e5
Tpalmer111 <mr.palmer111@gmail.com>
2022-09-29 11:30:29 -0600

    finished deliverable

---
+++ src/Fave.js
@@ -0,0 +1,35 @@
+// export default function Fave (props) {
+//     const handleClick = (e) => {
+//         e.stopPropagation()
+//         console.log("handling Fave Click!")
+//         props.onFaveToggle()
+//     }
+
+//     const action = props.isFave ? 'remove_from_queue' : 'add_to_queue'
+//         return (
+//             <div className={`film-row-fave ${action}`}
+//                 onClick={handleClick} 
+//             >
+//                 <p className="material-icons">add_to_queue</p>
+//             </div>
+//         )
+// }
+
+export default function Fave(props) {
+
+    const handleClick = e => {
+        e.stopPropagation()
+        console.log('handling fave click!')
+        props.onFaveToggle()
+    }
+
+    const action = props.isFave ? 'remove_from_queue' : 'add_to_queue' 
+
+    return (
+        <div className={`film-row-fave  ${action}`}
+            onClick={handleClick}
+        >
+            <p className="material-icons">add_to_queue</p>
+        </div>
+    )
+}
---
+++ src/Poster.js
@@ -0,0 +1,14 @@
+// export default function Poster (props) {
+//         return (
+//             <img src={`https://image.tmdb.org/t/p/w780/${props.poster_path}`} alt={`${props.title}'s movie poster`} />
+//         )
+// }
+
+export default function Poster(props) {
+    return (
+        <img 
+            src={`https://image.tmdb.org/t/p/w780/${props.poster_path}`} 
+            alt={`${props.title}'s movie poster`} 
+        />
+    )
+}
