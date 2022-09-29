dec63efb238d0eb98f2fa583a2820169098726ec
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 19:35:26 -0500

    Refactor App and FilmList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import './App.css';
 import FilmList from './FilmList';
 import Details from './Details';
 import TMDB from './TMDB';
 
 
-
-class App extends Component {
-  render() {
-    return (
-        <div className='App'>
-          <div className='film-library'>
+export default function App() {
+  return (
+    <div className='App'>
+        <div className='film-library'>
             <FilmList 
               films={TMDB.films}
             />
@@ -18,11 +16,30 @@ class App extends Component {
             <Details
               films={TMDB.films}
             />
-          </div>
-
         </div>
-    );
-  }
+
+    </div>    
+  )
 }
 
-export default App;
+
+// class App extends Component {
+//   render() {
+//     return (
+//         <div className='App'>
+//           <div className='film-library'>
+//             <FilmList 
+//               films={TMDB.films}
+//             />
+
+//             <Details
+//               films={TMDB.films}
+//             />
+//           </div>
+
+//         </div>
+//     );
+//   }
+// }
+
+// export default App;
