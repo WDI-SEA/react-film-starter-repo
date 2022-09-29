30e7207ed5909d8eed00ce54bdc907c1fcca3e00
waltersmatthew <waltersmatthew88@gmail.com>
2022-09-29 16:46:16 -0700

    finish deliverable

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import FilmList from './FilmList';
 import Details from './Details';
 import TMDB from './TMDB'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function App (){
-  const [films] = useState(TMDB.films)
+  const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrent] = useState({})
   
   const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+  
+  useEffect(() => {
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonDeets => {
+      console.log(jsonDeets)
+      setFilms(jsonDeets.results)
+    })
+  }, [])
+
   console.log(popularFilmsUrl)
   const handleDetailsClick = film => {
     // console.log(film)
