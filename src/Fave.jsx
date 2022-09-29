3c0a7e31fcc1115a40cd94e7b6582e2fd98d6b35
devin-lynch <devin.l.lynch@gmail.com>
2022-09-28 20:04:36 -0500

    Refactor Fave

--- src/Fave.jsx
+++ src/Fave.jsx
@@ -1,26 +1,48 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
+import { useState } from 'react'
 
-export default class Fave extends Component {
+export default function Fave(props) {
 
-    state = {
-        isFave: false
-    }
+    const [isFave, setIsFave] = useState(false)
 
-    handleClick = (e) => {
+    const handleClick = (e) => {
         e.stopPropagation()
         console.log("handling Fave click!")
-        this.setState(prevState => {
-            return {
-                isFave: !prevState.isFave
-            }
-        })
-    }
+        setIsFave(!isFave)
+    }    
 
-    render() {
-        return (
-            <div className={`film-row-fave ${this.state.isFave ? 'remove_from_queue' : 'add_to_queue'} `} onClick={this.handleClick}>
-                <p className="material-icons">add_to_queue</p>
-            </div>  
-        )
-    }
+    return (
+        <div className={`film-row-fave ${isFave ? 'remove_from_queue' : 'add_to_queue'} `} onClick={handleClick}>
+            <p className="material-icons">add_to_queue</p>
+        </div>          
+    )
 }
+
+
+
+
+
+// export default class Fave extends Component {
+
+//     state = {
+//         isFave: false
+//     }
+
+    // handleClick = (e) => {
+    //     e.stopPropagation()
+    //     console.log("handling Fave click!")
+    //     this.setState(prevState => {
+    //         return {
+    //             isFave: !prevState.isFave
+    //         }
+    //     })
+    // }
+
+//     render() {
+//         return (
+//             <div className={`film-row-fave ${this.state.isFave ? 'remove_from_queue' : 'add_to_queue'} `} onClick={this.handleClick}>
+//                 <p className="material-icons">add_to_queue</p>
+//             </div>  
+//         )
+//     }
+// }
