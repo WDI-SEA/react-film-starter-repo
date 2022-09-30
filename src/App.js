5babf3c7d72466e5a3eea9cb8fcf8dc5eefb3472
JasonXChen <jasonxchen888@gmail.com>
2022-09-29 20:35:30 -0400

    Fetch from TMDB API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 import FilmList from "./FilmList";
 import Details from "./Details";
 import TMDB from "./TMDB";
 
 function App() {
-    const [films] = useState(TMDB.films);
+    const [films, setFilms] = useState(TMDB.films);
     const [current, setCurrent] = useState({});
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    useEffect(() => {
+      const fetchFilms = async () => {
+        try {
+          console.log("UseEffect is firing!");
+          const response = await fetch(popularFilmsUrl);
+          const jsonData = await response.json();
+          setFilms(jsonData.results);
+        }
+        catch (err) {
+          console.warn(err);
+        }
+      }
+      fetchFilms();
+    }, []);
     const handleDetailsClick = film => {
         console.log(`info for ${film.title}`);
         setCurrent(film);
