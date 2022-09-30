e12e27b700969ea0ed1c3262ed33201ce5bf07d8
tatebonham <tatebonham@gmail.com>
2022-09-30 09:27:21 -0700

    Part 4 fixed, ran into an error that is now solved

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import { useState } from 'react';
-import './App.css';
-import FilmList from './FilmList';
-import Details from './Details';
-import TMDB from './TMDB';
+import { useState, useEffect } from 'react'
+import './App.css'
+import FilmList from './FilmList'
+import Details from './Details'
+import TMDB from './TMDB'
 
 export default function App() {
   const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrent] = useState({})
-  
-  const handleDetailsClick = film =>{
-    console.log('fetching details for ', film.title)
+
+  useEffect(() => {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`
+    fetch(url)
+      .then(response => response.json())
+      .then(jsonData => setFilms(jsonData.results))
+      .catch(console.warn)
+  }, [])
+
+  const handleDetailsClick = film => {
+    console.log('fetching details for film:', film.title)
     setCurrent(film)
   }
-    return (
-      <div className="App">
-        <div className="film-library">
-          <FilmList films={films} film={current} handleDetailsClick={handleDetailsClick} />
-          <Details films={films} film={current} />
-        </div>
-      </div>
-    );
-}
 
-;
+
+  console.log('api key:', TMDB.api_key)
+
+  return (
+    <div className="film-library">
+      <FilmList 
+        films={films} 
+        handleDetailsClick={handleDetailsClick}
+      />
+
+      <Details 
+        films={TMDB.films} 
+        film={current}
+      />
+    </div>
+  )
+}
