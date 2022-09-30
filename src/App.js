b97d36569223d7f83f5360209ab853b4e074ec70
ADRIANRROJO <rojoadrian16@gmail.com>
2022-09-30 11:18:22 -0500

    after hw review

--- src/App.js
+++ src/App.js
@@ -6,26 +6,26 @@ import TMDB from './TMDB'
 require('dotenv').config()
 
 
+
+
 export default function App() {
-  const url = `https://api.themoviedb.org/3/movie/?api_key=${TMDB.api_key}`
   const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrent] = useState({})
 
-  const handleDetailsClick = film => {
-    console.log('fetching details for film:', film.title)
-   
-    
-    setCurrent(film)
-}
+  
 
 useEffect(() => {
-  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
   fetch(popularFilmsUrl)
   .then(response => response.json())
-  .then(jsonDeets => {
-    console.log(jsonDeets)
-  })
+  .then(jsonData => setFilms(jsonData.results))
+    .catch(console.warn)
 }, [])
+const handleDetailsClick = film => {
+  console.log('fetching details for film:', film.title)
+  setCurrent(film)
+}
+
 return (
   <div className="film-library">
     <FilmList 
