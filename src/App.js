6bacb402f0b33ea3be8c42b31eeaa064e216b0b9
JamesOnwordi <Jamesonwordi50@gmail.com>
2022-09-29 20:50:26 -0500

    all done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React,{useState} from 'react'
+import {useState,useEffect} from 'react'
 import './App.css'
 import FilmList from './FilmList'
 import Details from './Details'
 import TMDB from './TMDB'
+import axios from 'axios'
 
 
 export default function App(){
 
-  const [films] = useState(TMDB.films)
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`
+console.log(TMDB.api_key)
+ 
+  const [films,setFilms] = useState(TMDB.films)
   const [current,setCurrent] = useState([])
-
+  
+  useEffect(()=>{
+    
+    axios.get(popularFilmsUrl)
+    .then(response =>{
+      console.log(response)
+      setFilms(response.data.results)
+    })
+  },[])
   const handleDetailsClick = film => {
     console.log('fetching details for film:', film.title)
     setCurrent(film)
