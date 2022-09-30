8178a9620bdfd875c178ca4714bf6350af503bed
devin-lynch <devin.l.lynch@gmail.com>
2022-09-29 20:38:37 -0500

    Complete pt 4

--- src/App.js
+++ src/App.js
@@ -2,14 +2,30 @@
 import './App.css';
 import FilmList from './FilmList';
 import Details from './Details';
-import TMDB from './TMDB';
-import { useState } from 'react'
+// import TMDB from './TMDB';
+import { useState, useEffect } from 'react'
 
 
 export default function App() {
 
+  const api_key = process.env.REACT_APP_TMDB_API_KEY
   const [current, setCurrent] = useState({})
-  const [films, setFilms] = useState(TMDB.films)
+  // const [films, setFilms] = useState(TMDB.films)
+  const [films, setFilms] = useState([])
+  
+  // console.log(api_key)
+  // console.log(popularFilmsUrl)
+  
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&language=en-US&page=1`;
+    console.log("UseEffect is firing!")
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+      setFilms(jsonData.results)
+    })
+  }, [])
 
   const handleDetailsClick = (film) => {
     console.log('Fetching details for', film.title)
