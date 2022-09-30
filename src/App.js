337cfd074529bdc50567b9a826581dab389f7471
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-09-29 17:17:31 -0700

    part 4 complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import FilmList from './FilmList'
 import Details from './Details'
 import TMDB from './TMDB'
+import axios from 'axios'
+
 
 export default function App() {
   const [films, setFilms] = useState(TMDB.films)
@@ -13,6 +15,16 @@ export default function App() {
     setCurrent(film)
 }
 
+
+useEffect(() => {
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_TMDB_API_KEY}&language=en-US&page=1`;
+  axios.get(popularFilmsUrl)
+    .then(response => {
+      // console.log(response)
+      setFilms(response.data.results)
+    })
+    .catch(console.warn)
+}, [])
 
   return (
     <div className="film-library">
