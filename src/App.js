99d5b8378ee48de0f38dce41859b352a599a83db
ninambash <ninambash@gmail.com>
2023-01-18 10:36:55 -0800

    updated

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Details from './Details';
+import FilmList from './FilmList';
+import TMDB from './TMDB';
+import { useState } from 'react';
+
+function App() {
+  const [films, setFilm] = useState(TMDB.films)
+  const [current, setCurrent] = useState({})
+
+  function handleDetailsClick(film) {
+    console.log('film details', film.title)
+    setCurrent(film)
+}
+    return (
+      <div className="film-library">
+
+      <FilmList films={films} handleDetailsClick={handleDetailsClick} />
+      <Details film={current}/>
+      </div>
+    );
+  }
+
+export default App;
