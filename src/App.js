88c80f7730ed6d59362102cf1add24bc2fd8b397
tylersb <tylersb@gmail.com>
2023-01-18 18:57:19 -0800

    finishes deliverable part 4

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import FilmList from './FilmList'
 import Details from './Details'
 import TMDB from './TMDB'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export default function App() {
-
-  const [films] = useState(TMDB.films)
+  const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrent] = useState([])
 
   const handleDetailsClick = (film) => {
     console.log('fetching details for film:', film.title)
     setCurrent(film)
   }
+  
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`
+    console.log('UseEffect is firing!')
+    fetch(popularFilmsUrl)
+      .then((response) => response.json())
+      .then((jsonData) => {
+        setFilms(jsonData.results)
+      })
+  }, [])
 
-    return (
-      <div className="film-library">
-        <FilmList films={films} handleDetailsClick={handleDetailsClick} />
-        <Details film={current} />
-      </div>
-    )
-  }
+  return (
+    <div className="film-library">
+      <FilmList films={films} handleDetailsClick={handleDetailsClick} />
+      <Details film={current} />
+    </div>
+  )
+}
