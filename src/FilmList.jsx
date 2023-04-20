7e4e7a2ffbaa1688f2f8f609e85089c91eada973
andrewbantly <andrewbantly@gmail.com>
2023-04-20 16:36:24 -0700

    part two complete

--- src/FilmList.jsx
+++ src/FilmList.jsx
@@ -2,6 +2,18 @@ import React, { Component } from "react";
 import FilmRow from "./FilmRow";
 
 export default class FilmList extends Component {
+
+    state = {
+        filter: 'all'
+    }
+
+    handleFilterClick = (filter) => {
+        console.log("a filter was clicked: " + filter)
+        this.setState({
+            filter
+        })
+    }
+
     render(){
         const filmRows = this.props.films.map((film, i) => (
             <FilmRow key={i} film={film}/>
@@ -9,6 +21,16 @@ export default class FilmList extends Component {
          return( 
             <div className="film-list">
             <h1 className="section-title">FILMS</h1>
+            <div className="film-list-filters">
+                <div className={`film-list-filter ${this.state.filter === "all" ? "is-active" : ""}`} onClick={() => this.handleFilterClick('all')}>
+                    ALL
+                    <span className="section-count">{this.props.films.length}</span>
+                </div>
+                <div className={`film-list-filter ${this.state.filter === "faves" ? "is-active" : ""}`} onClick={() => this.handleFilterClick('faves')}>
+                FAVES
+                <span className="section-count">0</span>
+                </div>
+            </div>
             {filmRows}
             </div>
             )
--- src/FilmRow.jsx
+++ src/FilmRow.jsx
@@ -3,14 +3,18 @@ import Poster from "./Poster"
 import Fave from "./Fave";
 
 export default class FilmRow extends Component {
+
+    handleDetailsClick = film => {
+        console.log(`fetching details for ${film.title}`)
+    }
+
     render() {
-        const { film } = this.props;
         return(
-            <div className="film-row">
-                <Poster poster={film.poster_path} title={film.title}/>
+            <div className="film-row" onClick={() => this.handleDetailsClick(this.props.film)}>
+                <Poster poster={this.props.film.poster_path} title={this.props.film.title}/>
                 <div className="film-summary">
-                    <h1>{film.title}</h1>
-                    <p>{film.release_date.split('-')[0]}</p>
+                    <h1>{this.props.film.title}</h1>
+                    <p>{this.props.film.release_date.split('-')[0]}</p>
                 </div>
                 <Fave/>
             </div>
