2fcd7ec5c99628f7da2999c40668d6c8d346c00f
andrewbantly <andrewbantly@gmail.com>
2023-04-24 18:55:15 -0700

    part four complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './App.css';
 import Details from "./Details";
 import FilmList from "./FilmList";
 import TMDB from "./TMDB";
 
 function App() {
-  const [films, setFilms] = useState(TMDB.films)
+  const [films, setFilms] = useState([])
   const [current, setCurrent] = useState({})
-
+  
   const handleDetailsClick = film => {
     console.log(`fetching details for ${film.title}`)
     setCurrent(film)
   }
+  
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData)
+      setFilms(jsonData.results)
+    })
+  }, [])
 
   return (
     <div className="film-library">
