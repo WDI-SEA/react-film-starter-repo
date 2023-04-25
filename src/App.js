05ee112be6055dcc87e7ccc1feb018efe5fc5b86
Powwow84 <pao.saelee84@gmail.com>
2023-04-24 23:05:49 -0700

    completed homework

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import './App.css';
 import Details from './Details';
 import FilmList from './FilmList';
@@ -8,6 +8,19 @@ function App() {
 
   const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrent] = useState({})
+
+  
+  useEffect(() => {
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    console.log("Use Effect is firing")
+    fetch(popularFilmsUrl)
+    .then(response => response.json())
+    .then(jsonData => {
+      setFilms(jsonData.results)
+    })
+  }, []) 
+
+  
 
   const  handleDetailsClick = film => {
     console.log(`fetching details for ${film.title}`)
