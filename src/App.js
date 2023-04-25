aac5922ec62de31db4fb796f9c0c47088299f579
brogers-97 <brburr24@gmail.com>
2023-04-25 09:42:59 -0700

    finished deliverable

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import FilmList from './FilmList';
 import Details from './Details';
 import TMDB from './TMDB';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
 
 
 export default function App() {
@@ -10,8 +11,24 @@ export default function App() {
   const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrent] = useState({})
 
+  useEffect(() => {
+    const movieRequest = async () => {
+      try{
+        const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+        const response = await axios.get(popularFilmsUrl)
+        console.log(response.data)
+        setFilms(response.data.results)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    movieRequest()
+  }, [])
+
   const handleDetailsClick = film => {
     console.log(`fetching details for ${film.title}`)
+    setCurrent(film)
   }
 
   return (
--- src/Details.jsx
+++ src/Details.jsx
@@ -34,6 +34,7 @@ export default function Details(props){
         return(
             <div className="film-details">
                 <h1 className="section-title">DETAILS</h1>
+                {details}
             </div>
         )
     
