cf1cd10ed1c4a23cbe7f1a1cbc8ce63fd406f857
Heiner000 <garrett.heiner@gmail.com>
2023-04-25 09:41:43 -0700

    switches from fetch to async await

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import './App.css';
 import FilmList from './FilmList'
 import Details from './Details'
 import { TMDB } from './TMDB'
+import axios from 'axios'
 
 export default function App() {
 
   const [films, setFilms] = useState(TMDB.films)
   const [current, setCurrent] = useState({})
+
   useEffect(() => {
     const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
 
-    fetch(popularFilmsUrl)
-      .then(response => response.json())
-      .then(jsonData => {
-        console.log(jsonData.results)
-        setFilms(jsonData.results)
-      })
+    // fetch(popularFilmsUrl)
+    //   .then(response => response.json())
+    //   .then(jsonData => {
+    //     console.log(jsonData.results)
+    //     setFilms(jsonData.results)
+    //   })
+    const movieRequest = async () => {
+      try {
+        const response = await axios.get(popularFilmsUrl)
+        console.log(response.data)
+        setFilms(response.data.results)
+      } catch(err) {
+        console.log(err)
+      }
+    }
 
+    movieRequest()
   }, [])
   
   const handleDetailsClick = (film) => {
